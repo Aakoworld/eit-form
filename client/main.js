1fd36fa20f420093d6e0297c2fe2a793a87b2ece
Funsho Olaniyi <funsholaniyi@gmail.com>
2018-09-24 11:22:12 +0000

    Added User Accounts Module

--- client/main.js
+++ client/main.js
@@ -1,5 +1,5 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+import '../imports/startup/accounts-config';
 import '/imports/ui/body.js';
 import '/imports/api/eits.js';
